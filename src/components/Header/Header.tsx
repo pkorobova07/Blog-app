bfbc64c0cef4f287a9d328ad0dcbb13578f64339
Arina Nov <arinanovikova710@gmail.com>
2025-12-04 01:06:02 +0600

    styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,15 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <Link href="/">DevBlog</Link>
-
+    <header className="site-header">
+      <div>
+        <h1 className="site-title">
+          <Link href="/">DevBlog</Link>
+        </h1>
+        <p className="site-subtitle">Пиши. Читай. Делись.</p>
+      </div>
       <nav>
-        <Link href="/create-post">+ Новый пост</Link>
+        <Link href="/create-post" className="post-button">+ Новый пост</Link>
       </nav>
     </header>
   );
